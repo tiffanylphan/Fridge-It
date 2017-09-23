56ffd50b8543eda0e009b76c214a2a19199b5a67
Daniel Chong <danchong625@gmail.com>
2017-09-22 19:48:11 -0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'client/public');
+const APP_DIR = path.resolve(__dirname, 'client/src');
+
+module.exports = {
+  entry: APP_DIR + 'index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  }
+};
