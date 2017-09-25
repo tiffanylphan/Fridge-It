d59c3bda0ad72c6d9ef209d142d2dc6cb40baf3c
Daniel Chong <danchong625@gmail.com>
2017-09-25 14:23:02 -0700

    [Init] Initialize reducers page for redux

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import your reducer pages here
+
+const FridgeApp = combineReducers({
+  // call your reducer pages here
+});
+
+export default FridgeApp;
