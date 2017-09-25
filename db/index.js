5260ddbdfbb72aa31cde1f320552dd62546d0ef5
Tiffan Phan <thetiffanyphan@gmail.com>
2017-09-25 12:03:34 -0700

    message routes exist

--- db/index.js
+++ db/index.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-<<<<<<< HEAD
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_URL);
@@ -7,23 +6,10 @@ const sequelize = new Sequelize(process.env.DB_URL);
 const Users = sequelize.define('user', {
   name: {
     type: Sequelize.STRING
-=======
-
-require('dotenv').config();
-const sequelize = new Sequelize(process.env.DB_URL);
-
-const Users = sequelize.define('users', {
-  name: {
-    type: sequelize.STRING
-  },
-  password: {
-    type: sequelize.STRING
->>>>>>> message controllers exist
   }
 })
 
 const Fridge = sequelize.define('fridge', {
-<<<<<<< HEAD
   users: {
     type: Sequelize.ARRAY({type: Sequelize.INTEGER})
   },
@@ -61,46 +47,8 @@ Users.hasMany(MessageInfo, {foreignKey: 'userId', allowNull: false, onDelete: 'C
 
 FridgeItems.belongsTo(Users, {foreignKey: 'userId', allowNull: false, onDelete: 'CASCADE'});
 Users.hasMany(FridgeItems, {foreignKey: 'userId', allowNull: false, onDelete: 'CASCADE'});
-=======
-  number: {
-    type: sequelize.INTEGER
-  }
-})
-
-const FridgeItems = sequelize.define('fridgeItems', {
-  name: {
-    type: sequelize.STRING
-  },
-  quantity: {
-    type: sequelize.INTEGER
-  },
-  type: {
-    type: sequelize.STRING
-  }
-})
-
-const MessageInfo = sequelize.define('messageBoard', {
-  messageText: {
-    type: sequelize.STRING
-  }
-})
-
-Users.belongsTo(Fridge);
-Fridge.hasMany(Users);
-Fridge.hasMany(FridgeItems);
-FridgeItems.belongsTo(Fridge);
-Fridge.hasMany(MessageInfo);
-MessageInfo.belongsTo(Fridge);
-
-sequelize.sync({force:false});
->>>>>>> message controllers exist
 
 module.exports.users = Users;
 module.exports.fridge = Fridge;
 module.exports.fridgeItems = FridgeItems;
 module.exports.messageInfo = MessageInfo;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> message controllers exist
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
-
+const messages = require('../controller/Messages');
 // List your routers here;
+router.get('/allMessages', messages.getMessages);
+router.post('/allMessages', messages.postMessages);
+router.delete('/allMessages/:id', messages.deleteMessages);
+router.patch('/allMessages/:id', messages.updateMessages);
 
 module.exports = router;
