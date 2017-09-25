221d4dd9f748bab29d4c0b6bc6691233f9485663
Tiffan Phan <thetiffanyphan@gmail.com>
2017-09-25 11:49:19 -0700

    message controllers exist

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+<<<<<<< HEAD
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_URL);
@@ -6,10 +7,23 @@ const sequelize = new Sequelize(process.env.DB_URL);
 const Users = sequelize.define('user', {
   name: {
     type: Sequelize.STRING
+=======
+
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.DB_URL);
+
+const Users = sequelize.define('users', {
+  name: {
+    type: sequelize.STRING
+  },
+  password: {
+    type: sequelize.STRING
+>>>>>>> message controllers exist
   }
 })
 
 const Fridge = sequelize.define('fridge', {
+<<<<<<< HEAD
   users: {
     type: Sequelize.ARRAY({type: Sequelize.INTEGER})
   },
@@ -47,8 +61,46 @@ Users.hasMany(MessageInfo, {foreignKey: 'userId', allowNull: false, onDelete: 'C
 
 FridgeItems.belongsTo(Users, {foreignKey: 'userId', allowNull: false, onDelete: 'CASCADE'});
 Users.hasMany(FridgeItems, {foreignKey: 'userId', allowNull: false, onDelete: 'CASCADE'});
+=======
+  number: {
+    type: sequelize.INTEGER
+  }
+})
+
+const FridgeItems = sequelize.define('fridgeItems', {
+  name: {
+    type: sequelize.STRING
+  },
+  quantity: {
+    type: sequelize.INTEGER
+  },
+  type: {
+    type: sequelize.STRING
+  }
+})
+
+const MessageInfo = sequelize.define('messageBoard', {
+  messageText: {
+    type: sequelize.STRING
+  }
+})
+
+Users.belongsTo(Fridge);
+Fridge.hasMany(Users);
+Fridge.hasMany(FridgeItems);
+FridgeItems.belongsTo(Fridge);
+Fridge.hasMany(MessageInfo);
+MessageInfo.belongsTo(Fridge);
+
+sequelize.sync({force:false});
+>>>>>>> message controllers exist
 
 module.exports.users = Users;
 module.exports.fridge = Fridge;
 module.exports.fridgeItems = FridgeItems;
 module.exports.messageInfo = MessageInfo;
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> message controllers exist
---
+++ server/controller/Messages.js
@@ -0,0 +1,46 @@
+const db = require('../../db');
+
+module.exports = {
+  getMessages: (req, res) => {
+    db.messageInfo.findAll({where: {fridgeId: req.body.fridgeId}})
+      .then(({data}) => {
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  },
+
+  postMessages: (req, res) => {
+    db.messageInfo.create()
+      .then(() => {
+        res.status(201).send('success');
+      })
+      .catch((err) => {
+        res.status(404).send(err);
+      });
+  },
+
+  updateMessages: (req, res) => {
+    db.messageInfo.find({where: {id: req.params.id}})
+      .then((data) => {
+        data.updateAttributes({
+          messageText: req.body.messageText 
+        })
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      })
+  },
+
+  deleteMessages: (req, res) => {
+    db.messageInfo.destroy({where: {id: req.params.id}})
+      .then(() => {
+        res.status(202).send('successfully deleted');
+      })
+      .catch((err) => {
+        res.status(404).send(err);
+      })
+  } 
+
+}
