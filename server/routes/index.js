c3e5a9b142764f5dee49339001068a3ff29f5230
Daniel Chong <danchong625@gmail.com>
2017-09-25 13:17:25 -0700

    [Update] Add search routes to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,23 +2,27 @@ const router = require('express').Router();
 const messages = require('../controller/messageController')
 const fridge = require('../controller/fridgeController')
 const item = require('../controller/itemController')
+const search = require('../controller/search');
 
-// List your routers here:
-//messages
+// Messages Routes
 router.get('/allMessages/:id', messages.getMessages)
 router.post('/allMessages', messages.postMessages)
 router.delete('/allMessages/:id', messages.deleteMessages)
 router.patch('/allMessages/:id', messages.updateMessages)
 
-//fridge
+// Fridge Routes
 router.post('/fridge:fridgeId', fridge.addFridge)
 router.get('/fridge/:fridgeId', fridge.getFridge)
 router.delete('/fridge/:fridgeId', fridge.deleteFridge)
 
-//items
+// Items Routes
 router.get('/items/:fridgeId', item.getAllItems)
 router.post('/items/:fridgeId', item.addItem)
 router.post('/items/:id', item.updateItem)
 router.delete('/items/:id', item.deleteItem)
+
+// Search Routes
+router.route('/search')
+  .put(search.getRecipes);
 
 module.exports = router;
--- server/routes/searchRoutes.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controller/search');
-
-router.route('/search')
-  .get(controller.getRecipes);
