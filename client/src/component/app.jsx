ea96781da9aba023329a5c59e62926ab7cb939f5
Daniel Chong <danchong625@gmail.com>
2017-09-25 14:22:46 -0700

    [Init] Initialize App component

---
+++ client/src/component/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hi</div>
+    )
+  }
+};
+
+export default App;
