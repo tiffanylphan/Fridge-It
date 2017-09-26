e1cd2f7a9327d7b766de1831c911d2f2f387d0ac
Daniel Chong <danchong625@gmail.com>
2017-09-26 12:06:25 -0700

    [Refactor] Clean up code

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,11 @@
 import { combineReducers } from 'redux';
+
 import message from './messageReducers';
-// import your reducer pages here
 import search from './searchReducers';
 import fridge from './fridgeReducers';
 import item from './itemReducers';
 
 const FridgeApp = combineReducers({
-  // call your reducer pages here
   message,
   search,
   fridge,
