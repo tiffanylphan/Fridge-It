a9fba9cb1326a21a24090b3f58f44412d88881d9
Daniel Chong <danchong625@gmail.com>
2017-09-25 18:45:05 -0700

    [Update] Update entry view to link correctly

--- client/src/component/searchListEntry.jsx
+++ client/src/component/searchListEntry.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SearchListEntry = (props) => (
-  <div id="card">
-    <img src={this.props.recipe.image} />
-    <h2>{this.props.recipe.title}</h2>
-  </div>
-);
+const SearchListEntry = (props) => {
+  let url = (this.props.recipe.title).split(' ').join('-');
+
+  return (
+    <div id="card">
+      <a href={"https://spoonacular.com/" + url + '-' + this.props.recipe.id}>
+        <img src={this.props.recipe.image} alt={this.props.recipe.title} />
+      </a>
+      <h1>{this.props.recipe.title}</h1>
+    </div>
+  );
+};
 
 export default connect(null, null)(SearchListEntry);
