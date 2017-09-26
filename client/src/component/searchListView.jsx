75e7dd19d1ae83475b39a047de856f64280bb2cb
Daniel Chong <danchong625@gmail.com>
2017-09-25 18:22:42 -0700

    [Update] Implement redux on Search List View component

--- client/src/component/searchListView.jsx
+++ client/src/component/searchListView.jsx
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import SearchListEntry from './searchListEntry';
+import searchActions from '../actions/searchActions.js';
 
 class SearchListView extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentWillMount() {
+    this.props.actions.fetchRecipes(this.props.ingredients);
+  }
+
   render() {
-    
+    const { recipeList, recipeFetched, recipeFetching } = this.props;
+
+    if (!recipeList.length) {
+      return (
+        <div>
+          {recipeList.map(recipe => {
+            <SearchListEntry {...this.props.actions} key={recipe.id} recipe={recipe} />
+          })}
+        </div>
+      )
+    }
   }
 };
 
-export default SearchListView;
+const mapState = (store) => {
+  return {
+    ingredients: store.fridgeItems, // TODO: update with correct prop name
+    recipeList: store.search.recipes,
+    recipeFetched: store.search.fetched,
+    recipeFetching: store.search.fetching,
+    recipeError: store.search.error,
+  }
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    actions: bindActionCreators(searchActions, dispatch),
+  }
+};
+
+export default connect(mapState, mapDispatch)(SearchListView);
