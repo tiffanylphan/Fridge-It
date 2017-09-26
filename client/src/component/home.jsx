f3498ae9fc5c85db20d1cb98e06250383ced408e
Tiffan Phan <thetiffanyphan@gmail.com>
2017-09-26 11:42:53 -0700

    [init] home component

---
+++ client/src/component/home.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Messages from './messagesListView';
+import Search from './searchListView';
+import Fridge from './fridgeListView';
+
+class Home extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    
+    <div>
+      
+    </div>
+
+    )
+  }
+
+}
+
+
+
