e424e3f05fa26c000e9e77afcae5f52e3526506c
Daniel Chong <danchong625@gmail.com>
2017-09-25 17:33:18 -0700

    [Init] Initialized Search List View component

---
+++ client/src/component/searchListView.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class SearchListView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    
+  }
+};
+
+export default SearchListView;
