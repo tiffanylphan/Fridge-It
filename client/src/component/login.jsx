ce3af00257a68e349fbf06ba6fc9a15162b80982
ajliss <aaron@lissismore.com>
2017-09-26 21:21:57 -0700

    Start function for saving username.

--- client/src/component/login.jsx
+++ client/src/component/login.jsx
@@ -4,48 +4,62 @@ import {loginWithGoogle} from '../firebase/auth.js'
 
 const firebaseAuthKey = "firebaseAuthInProgress";
 const appTokenKey = "appToken";
-
+const user = '';
 
 class Login extends React.Component {
   constructor (props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      user: JSON.parse(localStorage.getItem("user"))
+    }
+    this.handleGoogleLogin = this.handleGoogleLogin.bind(this);
   }
 
   // from example.
   handleGoogleLogin() {
-    console.log("handleGoogleLogin")
+    // console.log(localStorage)
     loginWithGoogle()
-      .catch(function (error) {
-        alert(error); // or show toast
-        localStorage.removeItem(firebaseAuthKey);
-      });
-      localStorage.setItem(firebaseAuthKey, "1");
+    .catch(function (error) {
+      alert(error); // or show toast
+      localStorage.removeItem(firebaseAuthKey);
+    });
+    localStorage.setItem(firebaseAuthKey, "1");
+    // need to get the username off the page and save it on local storage
+    localStorage.setItem(user, "username");
     }
 
   handleSubmit(event) {
     event.preventDefault();
     console.log('button was clicked')
-    this.handleGoogleLogin();
+    
   }
+
+  // ought to save username to localstorage.
+  // firebase.auth().onAuthStateChanged(function(user) {
+  //   localStorage.user = user; // user is undefined if no user signed in
+  // });
 
   render() {
     return (
+      <div>
     
     <div>
       <form >
         <div >
-          <label>First Name</label>
+          <label> First Name </label>
           <input type="text" placeholder="Username"></input>
         </div>
         <div >
-          <label>Last Name</label>
+          <label> Last Name </label>
           <input type="text" placeholder="Password"></input>
         </div>
         <button  type="button" onClick={this.handleSubmit}>Submit</button>
       </form>    
     </div>
-
+        <div>
+          <button onClick={this.handleGoogleLogin}>Sign in with Google</button>
+        </div>
+      </div>
     )
   }
 }
