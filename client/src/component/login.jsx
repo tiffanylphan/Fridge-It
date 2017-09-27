8d0c0edf1e231de6a59eab81e781fea13d146f82
ajliss <aaron@lissismore.com>
2017-09-26 20:45:11 -0700

    Start on google log in functionality.

--- client/src/component/login.jsx
+++ client/src/component/login.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
+import {loginWithGoogle} from '../firebase/auth.js'
+// import semantic from 'semantic-ui';
+
+const firebaseAuthKey = "firebaseAuthInProgress";
+const appTokenKey = "appToken";
+
 
 class Login extends React.Component {
   constructor (props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  
+  // from example.
+  handleGoogleLogin() {
+    console.log("handleGoogleLogin")
+    loginWithGoogle()
+      .catch(function (error) {
+        alert(error); // or show toast
+        localStorage.removeItem(firebaseAuthKey);
+      });
+      localStorage.setItem(firebaseAuthKey, "1");
+    }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('button was clicked')
+    this.handleGoogleLogin();
+  }
 
   render() {
     return (
     
     <div>
-      Log In
+      <form >
+        <div >
+          <label>First Name</label>
+          <input type="text" placeholder="Username"></input>
+        </div>
+        <div >
+          <label>Last Name</label>
+          <input type="text" placeholder="Password"></input>
+        </div>
+        <button  type="button" onClick={this.handleSubmit}>Submit</button>
+      </form>    
     </div>
 
     )
