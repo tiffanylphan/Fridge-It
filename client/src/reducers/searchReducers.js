fd865211e4cdba57e618ac94fb3482978c050d2c
Daniel Chong <danchong625@gmail.com>
2017-09-27 12:35:06 -0700

    [Fix] Fix spelling

--- client/src/reducers/searchReducers.js
+++ client/src/reducers/searchReducers.js
@@ -8,13 +8,14 @@ const initialState = {
 
 const searchReducer = (state=initialState, action) => {
   switch(action.type) {
-    case "FETCH_RECIPE": {
+    case "FETCH_RECIPES": {
       return Object.assign({}, state, {fetching: true});
     }
-    case "FETCH_RECIPE_REJECTED": {
+    case "FETCH_RECIPES_REJECTED": {
       return Object.assign({}, state, {fetching: false, error: action.payload});
     }
-    case "FETCH_RECIPE_FULFILLED": {
+    case "FETCH_RECIPES_FULFILLED": {
+      console.log('this is action.payload: ', action.payload);
       return Object.assign({}, state, {
         fetching: false,
         fetched: true,
