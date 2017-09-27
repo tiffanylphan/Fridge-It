29535f026c8cd1efb0633a120809da4881fa1161
Daniel Chong <danchong625@gmail.com>
2017-09-26 17:16:46 -0700

    [Update] Initialize redux on search view

--- client/src/component/searchView.jsx
+++ client/src/component/searchView.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as searchActions from '../actions/searchActions.js';
 
 class SearchView extends Component {
   constructor(props){
@@ -7,8 +10,24 @@ class SearchView extends Component {
   };
 
   render() {
-    
+    return (
+      <div>SEARCH VIEW</div>
+    )
   }
 };
 
-export default connect(null, null)(SearchView);
+const searchState = (store) => {
+  return {
+    ingredients: store.items.items,
+    recipeList: store.search.recipes,
+    filtered: store.search.filtered,
+  }
+};
+
+const searchDispatch = (dispatch) => {
+  return {
+    actions: bindActionCreators(searchActions, dispatch),
+  }
+};
+
+export default connect(searchState, searchDispatch)(SearchView);
