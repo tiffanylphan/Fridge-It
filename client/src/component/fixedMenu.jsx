f6d149d9bac2e9c6fea01bf6d77a433f65790d30
Tiffan Phan <thetiffanyphan@gmail.com>
2017-09-28 13:03:30 -0700

    [add] partial functionality to fixedMenu component. Needs correct routers

--- client/src/component/fixedMenu.jsx
+++ client/src/component/fixedMenu.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
-
 import {
   Button,
   Container,
   Menu,
 } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import { logout } from "../firebase/auth";
+
+import HomeView from './homeView.jsx';
+import Login from './login.jsx';
+import LandingPage from './landingPage.jsx';
+import SignUp from './signup.jsx';
+import * as authActions from '../actions/authActions.js';
 
 const FixedMenu = () => (
   <Menu fixed='top' size='large'>
     <Container>
-      <Menu.Item as='a' active>Home</Menu.Item>
-      <Menu.Item as='a'>Recipes</Menu.Item>
-      <Menu.Menu position='right'>
-        <Menu.Item className='item'>
-          <Button as='a'>Log in</Button>
-        </Menu.Item>
-        <Menu.Item>
-          <Button as='a' primary>Sign Up</Button>
-        </Menu.Item>
-      </Menu.Menu>
+      <Router>
+        <div>
+          <div>
+          <Menu.Item as='a' active>
+            <Link to="/">FRIDGE-IT</Link>
+          </Menu.Item>
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Button as='a'><Link to="/login">Log In</Link></Button>
+            </Menu.Item>
+            <Menu.Item>
+              <Button as='a'><Link to="/signup">Sign Up</Link></Button>
+            </Menu.Item>
+          </Menu.Menu>
+          </div>
+          <div>
+            <Route exact path="/" render={() => (<LandingPage />)} />
+            <Route path="/login" render={() => (<Login />)} />
+            <Route path="/signup" render={() => (<SignUp />)} />
+          </div>
+        </div>
+      </Router>
     </Container>
   </Menu>
 )
