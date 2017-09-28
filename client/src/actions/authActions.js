1f8d67db6a01adc47ea729a6fc8cba67295017aa
Daniel Chong <danchong625@gmail.com>
2017-09-28 11:27:29 -0700

    [Add] Add email signup action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -44,3 +44,15 @@ export const logoutUser = () => {
       });
   };
 };
+
+export const emailSignUp = (email, pw) => {
+  firebase.auth().createUserWithEmailAndPassword(email, pw)
+    .then(result => {
+      localStorage.setItem('name', result.email);
+      localStorage.setItem('userid', result.uid);
+      dispatch({type: 'USER_SIGNUP_FULFILLED', payload: result.email});
+    })
+    .catch(function(error) {
+      dispatch({type: 'USER_SIGNUP_REJECTED', payload: error.message});
+    })
+};
