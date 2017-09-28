8facbd9644187210e696df0bf0133f7c3b7e60ee
Daniel Chong <danchong625@gmail.com>
2017-09-27 20:47:07 -0700

    [Add] Add auth to reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,8 +4,10 @@ import message from './messageReducers';
 import search from './searchReducers';
 import fridge from './fridgeReducers';
 import items from './itemReducers';
+import auth from './authReducers';
 
 const FridgeApp = combineReducers({
+  auth,
   message,
   search,
   fridge,
