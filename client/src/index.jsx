1d130c516ec41fdc75d3b77f9516f4a54a414f0e
Daniel Chong <danchong625@gmail.com>
2017-09-28 13:32:03 -0700

    [Update] Implement redux router changes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import DOMReact from 'react-dom';
 import { Provider } from 'react-redux';
-
 import { applyMiddleware, createStore } from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk';
 import { createLogger as logger } from 'redux-logger';
 import promise from 'redux-promise-middleware';
 
 import FridgeApp from './reducers';
 import App from './component/app.jsx';
+import HomeView from './component/homeView.jsx';
+import LandingPage from './component/landingPage.jsx';
 
-const middleware = applyMiddleware(promise(), thunk, logger());
+const history = createHistory();
+const middleware = applyMiddleware(promise(), thunk, logger(), routerMiddleware(history));
 const store = createStore(FridgeApp, middleware);
 
 DOMReact.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('app')
 );
