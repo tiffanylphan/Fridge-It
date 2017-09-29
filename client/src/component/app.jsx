12ff0388737ed4b7cab3293266568a3b455b855c
Daniel Chong <danchong625@gmail.com>
2017-09-28 17:27:02 -0700

    [Update] Implement FixedMenu View

--- client/src/component/app.jsx
+++ client/src/component/app.jsx
@@ -8,6 +8,7 @@ import HomeView from './homeView.jsx';
 import Login from './login.jsx';
 import LandingPage from './landingPage.jsx';
 import SignUp from './signup.jsx';
+import FixedMenu from './fixedMenu.jsx';
 import * as authActions from '../actions/authActions.js';
 
 class App extends Component {
@@ -18,23 +19,12 @@ class App extends Component {
   render() {
     if(localStorage.getItem('userid')) {
       return (
-        <HomeView />
+        <HomeView history={this.props.history} />
       )
     } else {
       return (
         <div>
-          <Router>
-            <div>
-              <div className="ui pointing menu" >
-                <a className="item"><Link to="/">FRIDGE-IT</Link></a>
-                <a className="item"><Link to="/login">Login</Link></a>
-                <a className="item"><Link to="/signup">Sign Up</Link></a>
-              </div>
-                <Route exact path="/" render={() => (<LandingPage />)} />
-                <Route path="/login" render={() => (<Login />)} />
-                <Route path="/signup" render={() => (<SignUp />)} />
-            </div>
-          </Router>
+          <FixedMenu history={this.props.history}/>
         </div>
       );
     }
@@ -52,4 +42,4 @@ const appDispatch =(dispatch) => {
     actions: bindActionCreators(authActions, dispatch)
   }
 }
-export default withRouter(connect(appState, appDispatch)(App));
+export default connect(appState, appDispatch)(App);
