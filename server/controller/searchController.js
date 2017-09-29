74e2f5cb3c4255af682d651174de0c5a3c0cc28a
Daniel Chong <danchong625@gmail.com>
2017-09-29 11:28:23 -0700

    [Update] Update to reflect movement of example data

--- server/controller/searchController.js
+++ server/controller/searchController.js
@@ -1,15 +1,19 @@
 const unirest = require('unirest');
 require('dotenv').config();
 
-const temp = require('../../db/recipeDummy.json');
+const temp = require('../../db/seed/recipeDummy.json');
 
 const search = {
   getRecipes: (req, res) => {
     let ingredients = req.body.data.ingredients;
 
+<<<<<<< HEAD
     console.log(temp.data);
 
     res.status(200).send(temp.data);
+=======
+    res.send(temp.data);
+>>>>>>> [Update] Update to reflect movement of example data
 
     // unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients"
     //               + "?ingredients=" + ingredients.join(',') + '&fillIngredients=false'
