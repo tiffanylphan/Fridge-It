ab56d96d6c75999a447efe7131f3d998825487a5
Daniel Chong <danchong625@gmail.com>
2017-09-28 19:47:58 -0700

    [Temp] Create temp seeding data for recipe

--- server/controller/searchController.js
+++ server/controller/searchController.js
@@ -1,19 +1,25 @@
 const unirest = require('unirest');
 require('dotenv').config();
 
+const temp = require('../../db/recipeDummy.json');
+
 const search = {
   getRecipes: (req, res) => {
     let ingredients = req.body.data.ingredients;
 
-    unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients"
-                  + "?ingredients=" + ingredients.join(',') + '&fillIngredients=false'
-                  + "&limitLicense=false" + "&number=9" + "&ranking=1")
-            .header("X-Mashape-Key", process.env.API_FOOD_KEY)
-            .header("Accept", "application/json")
-            .end((result) => {
-              console.log('Headers: ', result.headers);
-              res.send(result.body);
-            });
+    console.log(temp.data);
+
+    res.send(temp.data);
+
+    // unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients"
+    //               + "?ingredients=" + ingredients.join(',') + '&fillIngredients=false'
+    //               + "&limitLicense=false" + "&number=9" + "&ranking=1")
+    //         .header("X-Mashape-Key", process.env.API_FOOD_KEY)
+    //         .header("Accept", "application/json")
+    //         .end((result) => {
+    //           console.log('Headers: ', result.headers);
+    //           res.send(result.body);
+    //         });
   },
 };
 
