1d30a2ec7c4885d4bac2de85834e9fc7ed3857ad
Tiffan Phan <thetiffanyphan@gmail.com>
2017-09-30 11:50:43 -0700

    [refactor] cleaned up code

--- client/src/component/messagesListView.jsx
+++ client/src/component/messagesListView.jsx
@@ -23,19 +23,12 @@ class MessageListView extends Component {
 
     const handleSubmit = () => {
       let inputMsg = document.getElementById('inputMsg');
-      // let inputMsg1 = document.getElementById('inputMsg1');
-      if (!inputMsg.value.match(/[a-z0-9_]/i) ) {
-        alert('Please enter a valid message');
-      } else {
+      if (inputMsg.value.match(/[a-z0-9_]/i) ) {
         this.props.actions.postMessages(fridge.id, username, inputMsg.value);
         inputMsg.value = '';
-        // inputMsg1.value = '';
       }
     }
 
-    // if (messageList.length > 0) {
-      // console.log('view before/after reducer: ', messageList);
-      // console.log('what is handlesubmit: ', handleSubmit);
       return (
         <div>
           <h3 className="ui dividing header">Messages</h3>
@@ -60,30 +53,6 @@ class MessageListView extends Component {
           ))}
         </div>
       )
-    // } else {
-    //   return (
-    //     <div>
-    //       <h3 className="ui dividing header">Messages</h3>
-    //       <Form>
-    //         <Form.Group inline>
-    //           <Form.Input
-    //           type="text" 
-    //           label="Message:"
-    //           id="inputMsg1"
-    //           placeholder="Enter message here" />
-    //           <Form.Button 
-    //             onClick={(e) => {
-    //               e.preventDefault();
-    //               handleSubmit();
-    //             }}
-    //             content="Submit"
-    //           />
-    //         </Form.Group>
-    //       </Form>
-    //     </div>
-
-    //   )
-    // }
   }
 };
 
