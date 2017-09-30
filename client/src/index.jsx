3ad53cc63e80e72a83fbd8702a0f6d1f48a5ac10
Daniel Chong <danchong625@gmail.com>
2017-09-30 14:59:06 -0700

    [Update] Attempt to autorehydrate

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import DOMReact from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import createHistory from 'history/createBrowserHistory';
 import { Router } from 'react-router-dom';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk';
 import { createLogger as logger } from 'redux-logger';
 import promise from 'redux-promise-middleware';
+import { autoRehydrate } from 'redux-persist';
 
 import FridgeApp from './reducers';
 import App from './component/app.jsx';
@@ -16,7 +17,11 @@ import LandingPage from './component/landingPage.jsx';
 
 const history = createHistory();
 const middleware = applyMiddleware(promise(), thunk, logger(), routerMiddleware(history));
-const store = createStore(FridgeApp, middleware);
+const enhancers = compose(
+    middleware,
+    autoRehydrate()
+);
+const store = createStore(FridgeApp, enhancers);
 
 DOMReact.render(
   <Provider store={store}>
