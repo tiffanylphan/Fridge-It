030bbcde1208672591771a07bf723e1cbb26b857
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-01 15:16:04 -0700

    [edit] for heroku

--- server/server.js
+++ server/server.js
@@ -27,10 +27,10 @@ app.get('*', (req, res) => {
 })
 
 // Server Initialization
-db.fridge.sync()
+db.fridge.sync({force:true})
   .then(() => {
-    db.messageInfo.sync();
-    db.fridgeItems.sync()
+    db.messageInfo.sync({force:true});
+    db.fridgeItems.sync({force:true})
       .then(() => {
         app.listen(port, () => {
           console.log('Listening on Port: ', port);
